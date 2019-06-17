1c16542bbea6b270fa8a6e2a22542c0d2c3c0a6c
Zech Heneveld <zechheneveld@Zechs-MacBook-Air.local>
2019-06-17 10:53:40 -0700

    initial commit

---
+++ src/main/java/com/example/demo/DataController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import java.time.LocalTime;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class DataController {
+
+	@GetMapping()
+	public String getTime() {
+		return "The Current Time Is: " + LocalTime.now().toString();
+	}
+}
